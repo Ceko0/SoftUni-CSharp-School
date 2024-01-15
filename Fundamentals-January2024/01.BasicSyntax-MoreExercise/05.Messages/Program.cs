316be680b908fcd82f7c17a8c0b3f405be5fcbbd
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-01-15 18:33:34 +0200

    new tasks

--- Fundamentals-January2024/01.BasicSyntax-MoreExercise/05.Messages/Program.cs
+++ Fundamentals-January2024/01.BasicSyntax-MoreExercise/05.Messages/Program.cs
@@ -5,19 +5,19 @@
         static void Main(string[] args)
         {
             int counter = int.Parse(Console.ReadLine());
-            string outPutLetter = "";
+            string outputLetter = "";
             for (int i = 0; i < counter; i++)
             {
                 int digit = int.Parse(Console.ReadLine());
-                if (digit == 0) { outPutLetter += " "; continue; }
+                if (digit == 0) { outputLetter += " "; continue; }
                 int digitLenght = digit.ToString().Length;
                 int mainDigit = digit % 10;
                 int letterDigit = ((mainDigit - 2) * 3) + digitLenght - 1 + 97;
                 if (mainDigit >= 8) letterDigit++;
                 char letter = (char)letterDigit;
-                outPutLetter += letter;
+                outputLetter += letter;
             }
-            Console.WriteLine(outPutLetter);
+            Console.WriteLine(outputLetter);
         }
     }
 }
