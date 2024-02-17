d3c10bab9e70449dc82ff41399fe426b45d392c2
Tsvetomir Ivanov <155464069+Ceko0@users.noreply.github.com>
2024-02-17 19:48:19 +0200

    new solutions

--- Fundamentals-January2024/04.MethodsMoreExercise/03.LongerLine/Program.cs
+++ Fundamentals-January2024/04.MethodsMoreExercise/03.LongerLine/Program.cs
@@ -12,52 +12,86 @@
             double y3 = double.Parse(Console.ReadLine());
             double x4 = double.Parse(Console.ReadLine());
             double y4 = double.Parse(Console.ReadLine());
-            CheckTheLongestLine(x1,y1,x2,y2,x3,y3,x4,y4);
-        }
 
-        private static void CheckTheLongestLine(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
-        {
-            double x1y1 = XAndYCalculator(x1, y1);
-            double x2y2 = XAndYCalculator(x2, y2);
-            double x3y3 = XAndYCalculator(x3, y3);
-            double x4y4 = XAndYCalculator(x4, y4);
+            double firstLine = NewMethod(x1, x2) + NewMethod(y1, y2);
+            double secondLine = NewMethod(x3, x4) + NewMethod(y3, y4);
 
-            double firstLine = LineCalculator(x1y1, x2y2); ;
-            double secondLine = LineCalculator(x3y3, x4y4); ;
-
-            if (firstLine >= secondLine)
+            if (firstLine > secondLine)
             {
-                CloserToCenterPrint(x1y1, x2y2 , x1 , y1 , x2 , y2);
+                if (CloserToZeroCheck(x1, y1, x2, y2))
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+
             }
             else
             {
-                CloserToCenterPrint(x3y3, x4y4, x3, y3, x4, y4);
+                if (!CloserToZeroCheck(x3, y3, x4, y4))
+                {
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+                }
+
             }
         }
 
-        private static void CloserToCenterPrint(double x1y1, double x2y2, double x1, double y1, double x2, double y2)
+        private static bool CloserToZeroCheck(double x1, double y1, double x2, double y2)
         {
-            if (x1y1 < x2y2) Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
-            else Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+            if (x1 < x2)
+            {
+                if (y1 < y2)
+                {
+                    return true;
+                }
+                else if (x1 < y2)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if (y1 < y2)
+                {
+                    if (x2 > y1)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
-        private static double XAndYCalculator(double x, double y)
+        private static double NewMethod(double x1, double x2)
         {
-            //double xy = 0;
-            //if ((x > 0 && y > 0) || (x < 0 && y < 0)) xy = x + y;
-            //else if ((x <= 0 && y >= 0) || (x >= 0 && y <= 0)) xy = x + y;
-            return x + y;
+            double sideOne = 0;
+            if (biger(x1, x2))
+            {
+                sideOne = x1 - x2;
+            }
+            else
+            {
+                sideOne = x2 - x1;
+            }
+
+            return Math.Pow(sideOne, 2);
         }
-        private static double LineCalculator(double x1y1, double x2y2)
+
+        private static bool biger(double x1, double x2)
         {
-            //double line = 0;
-            //if ((x1y1 > 0 && x2y2 > 0) || (x1y1 < 0 && x2y2 < 0))
-            //{
-            //    if (x1y1 > x2y2) line = x1y1 - x2y2;
-            //    else line = x2y2 - x1y1;
-            //}
-            //if ((x1y1 >= 0 && x2y2 <= 0) || (x1y1 <= 0 && x2y2 >= 0)) line = Math.Abs(x1y1 + x2y2);
-            return x1y1 + x2y2;
+            if (x1 > x2)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
